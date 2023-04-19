194b0537a6fb597d8f9d76d720e4cdd81d3fe4ee
Sean <sean.nuttall@gmail.com>
2023-04-19 13:51:40 -0700

    working on adding products

--- force-app/main/default/lwc/productList/productList.js
+++ force-app/main/default/lwc/productList/productList.js
@@ -285,7 +285,7 @@ export default class ProductList extends LightningElement {
         console.log(this.whatever);
         console.log(this.product);
         console.log(this.quantity);
-        await createProduct({
+        createProduct({
             OrderId: this.whatever,
             ProductId: this.product,
             Quant: this.quantity,
@@ -295,9 +295,6 @@ export default class ProductList extends LightningElement {
             type: "ordSubmit"
         };
         publish(this.messageContext, NAME_SELECTED_CHANNEL, payload);
-        const orderId = this.whatever.map((item) =>{
-            return { recordId: item };
-        })
-        await notifyRecordUpdateAvailable(orderId);
+        
     }
 }
